ec0b49b9272be12c62c59a60e7eee3387a3d86c2
Craig Morris <craig.michael.morris@gmail.com>
2019-06-18 21:33:45 +0200

    fix(config): use env variables by default

--- config/eventstore.php
+++ config/eventstore.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'tcp_url' => 'tls://admin:changeit@localhost:1113',
-    'http_url' => 'http://admin:changeit@localhost:2113',
+    'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@localhost:1113'),
+    'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@localhost:2113'),
     'streams' => [],
     'group' => '',
     'namespace' => 'App\Events'
