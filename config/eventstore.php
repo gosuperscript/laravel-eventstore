c82078c26a6eb58d197af6793ab44d97f771553e
Kani Robinson <kani.robinson@digitalrisks.co.uk>
2019-08-09 16:38:20 +0100

    Added volatile configuration

--- config/eventstore.php
+++ config/eventstore.php
@@ -4,6 +4,7 @@ return [
     'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@localhost:1113'),
     'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@localhost:2113'),
     'streams' => [],
+    'volatile_streams' => [],
     'group' => '',
     'namespace' => 'App\Events'
 ];
