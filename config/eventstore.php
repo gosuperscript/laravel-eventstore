8060a4cae60a437f7e6955609910795e1625e7a4
KaniRobinson <kani_westham@hotmail.com>
2019-08-22 11:13:30 +0100

    Added key to config/eventstore.php

--- config/eventstore.php
+++ config/eventstore.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'key' => env('EVENTSTORE_KEY', null),
     'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@localhost:1113'),
     'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@localhost:2113'),
     'streams' => [],
