9a737276c0e533caed5d4c89880f8af0211802ab
Craig Morris <craig.morris@digitalrisks.co.uk>
2019-10-16 11:35:16 +0100

    fix(config): Use env vars in default config.php
    So we don't *have* to copy this file into our config directory.

--- config/eventstore.php
+++ config/eventstore.php
@@ -3,9 +3,9 @@
 return [
     'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@localhost:1113'),
     'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@localhost:2113'),
-    'subscription_streams' => [],
-    'volatile_streams' => [],
-    'group' => '',
+    'subscription_streams' => array_filter(explode(',', env('EVENTSTORE_SUBSCRIPTION_STREAMS'))),
+    'volatile_streams' => array_filter(explode(',', env('EVENTSTORE_VOLATILE_STREAMS'))),
+    'group' => env('EVENTSTORE_SUBSCRIPTION_GROUP', env('APP_NAME', 'laravel')),
     'event_to_class' => function ($event) {
         return 'App\Events\\' . $event->getType();
     }
