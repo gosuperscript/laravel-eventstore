26a0d08c6b9e28ddda280f34e4249c1b778b2cde
Pawel Trauth <pawel.trauth@digitalrisks.co.uk>
2019-12-05 15:06:54 +0000

    add connection attribute to allow for events to trigger listeners without going through ES

--- src/Traits/ReceivedFromEventStore.php
+++ src/Traits/ReceivedFromEventStore.php
@@ -2,11 +2,8 @@
 
 namespace DigitalRisks\LaravelEventStore\Traits;
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
-use ReflectionClass;
-use ReflectionProperty;
 use Rxnet\EventStore\Record\EventRecord;
+use Rxnet\EventStore\Data\EventRecord as EventRecordData;
 
 trait ReceivedFromEventStore
 {
@@ -14,6 +11,15 @@ trait ReceivedFromEventStore
 
     public function getEventRecord(): ?EventRecord
     {
+        if (config('eventstore.connection') == 'sync') {
+            $properties = get_object_vars($this);
+            unset($properties['event']);
+
+            return new EventRecord(new EventRecordData([
+                'data' => json_encode($properties)
+            ]));
+        }
+
         return $this->event;
     }
 
