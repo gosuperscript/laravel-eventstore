6177c8ddab6d8f3c0545b2784bffcee518f690be
Craig Morris <craig.michael.morris@gmail.com>
2021-01-22 16:27:22 +0000

    feature: allow guzzle 7

--- config/eventstore.php
+++ config/eventstore.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@localhost:1113'),
-    'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@localhost:2113'),
+    'tcp_url' => env('EVENTSTORE_TCP_URL', 'tls://admin:changeit@127.0.0.1:1113'),
+    'http_url' => env('EVENTSTORE_HTTP_URL', 'http://admin:changeit@127.0.0.1:2113'),
     'subscription_streams' => array_filter(explode(',', env('EVENTSTORE_SUBSCRIPTION_STREAMS'))),
     'volatile_streams' => array_filter(explode(',', env('EVENTSTORE_VOLATILE_STREAMS'))),
     'group' => env('EVENTSTORE_SUBSCRIPTION_GROUP', env('APP_NAME', 'laravel')),
